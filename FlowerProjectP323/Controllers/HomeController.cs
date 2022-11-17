91c25c3f7f564f110184dc9507463e8dda091f99
project2003 <projectalgoritmika123@gmail.com>
2022-11-17 17:39:55 +0400

    added

---
+++ FlowerProjectP323/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FlowerProjectP323.DAL;
+using FlowerProjectP323.ViewModel.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerProjectP323.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(AppDbContext  appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task <IActionResult> Index()
+        {
+            Response.Cookies.Append("test", "data", new CookieOptions
+            {
+                MaxAge = TimeSpan.FromMinutes(10)
+            });
+            var model = new HomeIndexViewModel
+            { 
+             Products= await _appDbContext.Products.OrderByDescending(p =>p.Id).Take(8).ToListAsync()
+            };
+            return View(model);
+        }
+    }
+}
